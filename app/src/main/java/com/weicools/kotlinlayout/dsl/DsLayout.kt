b69a5ab5030cd9da4422652bec672b09fc73ee86
zhengwei.zhang <zhengwei.zhang@enerjoy.life>
2020-05-14 16:59:05 +0800

    move extends view and method

--- app/src/main/java/com/weicools/kotlinlayout/dsl/DsLayout.kt
+++ app/src/main/java/com/weicools/kotlinlayout/dsl/DsLayout.kt
@@ -2,24 +2,142 @@
 
 package com.weicools.kotlinlayout.dsl
 
+import android.content.Context
+import android.content.res.Resources
 import android.graphics.Color
+import android.graphics.Typeface
 import android.util.TypedValue
-import android.view.View
-import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.RelativeLayout
-import android.widget.TextView
+import android.view.*
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.content.res.AppCompatResources
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.constraintlayout.helper.widget.Flow
+import androidx.constraintlayout.helper.widget.Layer
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintProperties
+import androidx.constraintlayout.widget.Guideline
 import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.MarginLayoutParamsCompat
+import androidx.core.widget.NestedScrollView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 
 /**
  * @author weicools
  * @date 2020.05.14
  */
+
+//<editor-fold desc="ViewGroup widget creation function">
+inline fun ViewGroup.View(init: View.() -> Unit) =
+  View(context).apply(init).also { addView(it) }
+
+inline fun ViewGroup.TextView(init: AppCompatTextView.() -> Unit) =
+  AppCompatTextView(context).apply(init).also { addView(it) }
+
+inline fun ViewGroup.ImageView(init: AppCompatImageView.() -> Unit) =
+  AppCompatImageView(context).apply(init).also { addView(it) }
+
+inline fun ViewGroup.Button(init: Button.() -> Unit) =
+  AppCompatButton(context).apply(init).also { addView(it) }
+
+inline fun ViewGroup.EditText(init: EditText.() -> Unit) =
+  EditText(context).apply(init).also { addView(it) }
+
+inline fun ConstraintLayout.Guideline(init: Guideline.() -> Unit) =
+  Guideline(context).apply(init).also { addView(it) }
+
+inline fun ConstraintLayout.Flow(init: Flow.() -> Unit) =
+  Flow(context).apply(init).also { addView(it) }
+
+inline fun ConstraintLayout.Layer(init: Layer.() -> Unit) =
+  Layer(context).apply(init).also { addView(it) }
+
+inline fun ViewGroup.ConstraintLayout(init: ConstraintLayout.() -> Unit) =
+  ConstraintLayout(context).apply(init).also { addView(it) }
+
+inline fun ViewGroup.LinearLayout(init: LinearLayout.() -> Unit) =
+  LinearLayout(context).apply(init).also { addView(it) }
+
+inline fun ViewGroup.FrameLayout(init: FrameLayout.() -> Unit) =
+  FrameLayout(context).apply(init).also { addView(it) }
+
+inline fun ViewGroup.RelativeLayout(init: RelativeLayout.() -> Unit) =
+  RelativeLayout(context).apply(init).also { addView(it) }
+
+inline fun ViewGroup.NestedScrollView(init: NestedScrollView.() -> Unit) =
+  NestedScrollView(context).apply(init).also { addView(it) }
+
+inline fun ViewGroup.RecyclerView(init: RecyclerView.() -> Unit) =
+  RecyclerView(context).apply(init).also { addView(it) }
+//</editor-fold>
+
+//<editor-fold desc="Context widget creation function">
+inline fun Context.View(init: View.() -> Unit) =
+  View(this).apply(init)
+
+inline fun Context.TextView(init: TextView.() -> Unit) =
+  AppCompatTextView(this).apply(init)
+
+inline fun Context.ImageView(init: ImageView.() -> Unit) =
+  AppCompatImageView(this).apply(init)
+
+inline fun Context.Button(init: Button.() -> Unit) =
+  AppCompatButton(this).apply(init)
+
+inline fun Context.ConstraintLayout(init: ConstraintLayout.() -> Unit) =
+  ConstraintLayout(this).apply(init)
+
+inline fun Context.LinearLayout(init: LinearLayout.() -> Unit) =
+  LinearLayout(this).apply(init)
+
+inline fun Context.FrameLayout(init: FrameLayout.() -> Unit) =
+  FrameLayout(this).apply(init)
+
+inline fun Context.RelativeLayout(init: RelativeLayout.() -> Unit) =
+  RelativeLayout(this).apply(init)
+
+inline fun Context.NestedScrollView(init: NestedScrollView.() -> Unit) =
+  NestedScrollView(this).apply(init)
+
+inline fun Context.RecyclerView(init: RecyclerView.() -> Unit) =
+  RecyclerView(this).apply(init)
+//</editor-fold>
+
+//<editor-fold desc="Fragment widget creation function">
+inline fun Fragment.View(init: View.() -> Unit) =
+  context?.let { View(it).apply(init) }
+
+inline fun Fragment.TextView(init: TextView.() -> Unit) =
+  AppCompatTextView(context).apply(init)
+
+inline fun Fragment.ImageView(init: ImageView.() -> Unit) =
+  AppCompatImageView(context).apply(init)
+
+inline fun Fragment.Button(init: Button.() -> Unit) =
+  AppCompatButton(context).apply(init)
+
+inline fun Fragment.ConstraintLayout(init: ConstraintLayout.() -> Unit) =
+  ConstraintLayout(context).apply(init)
+
+inline fun Fragment.LinearLayout(init: LinearLayout.() -> Unit) =
+  LinearLayout(context).apply(init)
+
+inline fun Fragment.FrameLayout(init: FrameLayout.() -> Unit) =
+  context?.let { FrameLayout(it).apply(init) }
+
+inline fun Fragment.RelativeLayout(init: RelativeLayout.() -> Unit) =
+  RelativeLayout(context).apply(init)
+
+inline fun Fragment.NestedScrollView(init: NestedScrollView.() -> Unit) =
+  context?.let { NestedScrollView(it).apply(init) }
+
+inline fun Fragment.RecyclerView(init: RecyclerView.() -> Unit) =
+  context?.let { RecyclerView(it).apply(init) }
+//</editor-fold>
 
 //<editor-fold desc="View extend field">
 inline var View.ds_layout_width: Int
@@ -494,4 +612,159 @@ inline var View.ds_relative_centerVertical: Boolean
       addRule(RelativeLayout.CENTER_VERTICAL, RelativeLayout.TRUE)
     }
   }
+//</editor-fold>
+
+//<editor-fold desc="NestedScrollView extend field">
+@Suppress("UNUSED_PARAMETER")
+inline var NestedScrollView.fadeScrollBar: Boolean
+  get() {
+    return false
+  }
+  set(value) {
+    isScrollbarFadingEnabled = true
+  }
+//</editor-fold>
+
+//<editor-fold desc="Click extend field">
+var View.onClick: (View) -> Unit
+  get() {
+    return {}
+  }
+  set(value) {
+    setOnClickListener { v -> value(v) }
+  }
+
+var RecyclerView.onItemClick: (View, Int) -> Unit
+  get() {
+    return { _, _ -> }
+  }
+  set(value) {
+    setOnItemClickListener(value)
+  }
+//</editor-fold>
+
+//<editor-fold desc="View layout constant">
+const val match_parent = ViewGroup.LayoutParams.MATCH_PARENT
+const val wrap_content = ViewGroup.LayoutParams.WRAP_CONTENT
+
+const val horizontal = LinearLayout.HORIZONTAL
+const val vertical = LinearLayout.VERTICAL
+
+const val bold = Typeface.BOLD
+const val normal = Typeface.NORMAL
+const val italic = Typeface.ITALIC
+const val bold_italic = Typeface.BOLD_ITALIC
+
+const val gravity_start = Gravity.START
+const val gravity_end = Gravity.END
+const val gravity_top = Gravity.TOP
+const val gravity_bottom = Gravity.BOTTOM
+const val gravity_center = Gravity.CENTER
+const val gravity_center_horizontal = Gravity.CENTER_HORIZONTAL
+const val gravity_center_vertical = Gravity.CENTER_VERTICAL
+
+val scale_fix_xy = ImageView.ScaleType.FIT_XY
+val scale_center_crop = ImageView.ScaleType.CENTER_CROP
+val scale_center = ImageView.ScaleType.CENTER
+val scale_center_inside = ImageView.ScaleType.CENTER_INSIDE
+val scale_fit_center = ImageView.ScaleType.FIT_CENTER
+val scale_fit_end = ImageView.ScaleType.FIT_END
+val scale_matrix = ImageView.ScaleType.MATRIX
+val scale_fit_start = ImageView.ScaleType.FIT_START
+
+const val constraint_start = ConstraintProperties.START
+const val constraint_end = ConstraintProperties.END
+const val constraint_top = ConstraintProperties.TOP
+const val constraint_bottom = ConstraintProperties.BOTTOM
+const val constraint_baseline = ConstraintProperties.BASELINE
+const val constraint_parent = ConstraintProperties.PARENT_ID
+
+const val spread = ConstraintLayout.LayoutParams.CHAIN_SPREAD
+const val packed = ConstraintLayout.LayoutParams.CHAIN_PACKED
+const val spread_inside = ConstraintLayout.LayoutParams.CHAIN_SPREAD_INSIDE
+
+const val wrap_none = Flow.WRAP_NONE
+const val wrap_chain = Flow.WRAP_CHAIN
+const val wrap_aligned = Flow.WRAP_ALIGNED
+
+const val parent_id = ConstraintLayout.LayoutParams.PARENT_ID
+const val parent_ids = "0"
+//</editor-fold>
+
+//<editor-fold desc="layout helper function">
+fun Int.dp(): Int =
+  TypedValue.applyDimension(
+    TypedValue.COMPLEX_UNIT_DIP, this.toFloat(),
+    Resources.getSystem().displayMetrics
+  ).toInt()
+
+fun Float.dp(): Float =
+  TypedValue.applyDimension(
+    TypedValue.COMPLEX_UNIT_DIP, this,
+    Resources.getSystem().displayMetrics
+  )
+
+fun ViewGroup.MarginLayoutParams.toConstraintLayoutParam() =
+  ConstraintLayout.LayoutParams(width, height).also {
+    it.topMargin = this.topMargin
+    it.bottomMargin = this.bottomMargin
+    it.marginStart = this.marginStart
+    it.marginEnd = this.marginEnd
+  }
+
+fun ViewGroup.LayoutParams.append(set: ConstraintLayout.LayoutParams.() -> Unit) =
+  (this as? ConstraintLayout.LayoutParams)?.apply(set) ?: (this as? ViewGroup.MarginLayoutParams)?.toConstraintLayoutParam()?.apply(set)
+
+fun String.toLayoutId(): Int {
+  var id = java.lang.String(this).bytes.sum()
+  if (id == 48) id = 0
+  return id
+}
+
+fun <T : View> View.findView(id: Int): T? = findViewById(id)
+fun <T : View> AppCompatActivity.findView(id: Int): T? = findViewById(id)
+
+fun <T : View> View.find(id: String): T = findViewById(id.toLayoutId())
+
+fun <T : View> AppCompatActivity.find(id: String): T = findViewById(id.toLayoutId())
+
+fun RecyclerView.setOnItemClickListener(listener: (View, Int) -> Unit) {
+  addOnItemTouchListener(object : RecyclerView.OnItemTouchListener {
+    val gestureDetector = GestureDetector(context, object : GestureDetector.OnGestureListener {
+      override fun onShowPress(e: MotionEvent?) {}
+
+      override fun onSingleTapUp(e: MotionEvent?): Boolean {
+        e?.let {
+          findChildViewUnder(it.x, it.y)?.let { child ->
+            listener(child, getChildAdapterPosition(child))
+          }
+        }
+        return false
+      }
+
+      override fun onDown(e: MotionEvent?): Boolean {
+        return false
+      }
+
+      override fun onFling(e1: MotionEvent?, e2: MotionEvent?, velocityX: Float, velocityY: Float): Boolean {
+        return false
+      }
+
+      override fun onScroll(e1: MotionEvent?, e2: MotionEvent?, distanceX: Float, distanceY: Float): Boolean {
+        return false
+      }
+
+      override fun onLongPress(e: MotionEvent?) {}
+    })
+
+    override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) {}
+
+    override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
+      gestureDetector.onTouchEvent(e)
+      return false
+    }
+
+    override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {}
+  })
+}
 //</editor-fold>
