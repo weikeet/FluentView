97cf7f31c8ea4455a51e0301177dfbc2e73e331e
zhengwei.zhang <zhengwei.zhang@enerjoy.life>
2021-04-21 17:25:27 +0800

    忽略非 xml 布局的 View

--- app/src/main/java/com/weicools/kotlinlayout/MainActivity.kt
+++ app/src/main/java/com/weicools/kotlinlayout/MainActivity.kt
@@ -16,16 +16,34 @@ class MainActivity : AppCompatActivity() {
     private const val TAG = "TEST_TAG"
   }
 
+  private val ignoreNameList: MutableList<String> = mutableListOf<String>().apply {
+    add("LinearLayout")
+    add("ViewStub")
+    add("FrameLayout")
+    add("androidx.appcompat.widget.ActionBarOverlayLayout")
+    add("androidx.appcompat.widget.ContentFrameLayout")
+    add("androidx.appcompat.widget.ActionBarContainer")
+    add("androidx.appcompat.widget.Toolbar")
+    add("androidx.appcompat.widget.ActionBarContextView")
+  }
+
   private var inflateTimeUs: Long = 0L
 
   private fun installFactory2() {
     LayoutInflaterCompat.setFactory2(LayoutInflater.from(this), object : LayoutInflater.Factory2 {
       override fun onCreateView(parent: View?, name: String, context: Context, attrs: AttributeSet): View? {
+        if (ignoreNameList.size > 0 && ignoreNameList.contains(name)) {
+          ignoreNameList.remove(name)
+          return delegate.createView(parent, name, context, attrs)
+        }
+
         val startNs = System.nanoTime()
         val view = delegate.createView(parent, name, context, attrs)
+        val viewName = view?.let { it::class.simpleName }
         val costUs = (System.nanoTime() - startNs) / 1000
+
         inflateTimeUs += costUs
-        Log.d(TAG, "name=$name view=${view?.let { it::class.simpleName }} costUs=$costUs  sumUs=$inflateTimeUs")
+        Log.d(TAG, "name=$name view=$viewName costUs=$costUs  sumUs=$inflateTimeUs")
         return view
       }
 
@@ -41,15 +59,15 @@ class MainActivity : AppCompatActivity() {
 
     // setContentView(R.layout.activity_traditional_layout)
     // val contentLayout: View = findViewById(R.id.contentLayout)
-    // setContentView(R.layout.activity_constraint_layout)
-    // val contentLayout: View = findViewById(R.id.contentLayout)
+    setContentView(R.layout.activity_constraint_layout)
+    val contentLayout: View = findViewById(R.id.contentLayout)
     // setContentView(R.layout.activity_constraint_multi_layout)
     // val contentLayout: View = findViewById(R.id.contentLayout)
 
     // val contentLayout = MainConstraintLayout(this)
     // setContentView(contentLayout)
-    val contentLayout = MainConstraintLayout2(this)
-    setContentView(contentLayout)
+    // val contentLayout = MainConstraintLayout2(this)
+    // setContentView(contentLayout)
     // val contentLayout = MainConstraintMultiLayout2(this)
     // setContentView(contentLayout)
 
