e8b1f32c1b76fa0bc6d39bd78712c1689bbdf435
zhengwei.zhang <zhengwei.zhang@enerjoy.life>
2021-09-19 12:54:25 +0800

    released 1.0.0

--- app/src/main/java/com/weicools/fluentlayout/MainConstraintLayout2.kt
+++ app/src/main/java/com/weicools/fluentlayout/MainConstraintLayout2.kt
@@ -28,7 +28,6 @@ import com.weicools.fluent.widget.params.wrapContent
 class MainConstraintLayout2 : ConstraintLayout {
   constructor(context: Context) : super(context)
   constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
-  constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
   init {
     createView()
--- app/src/main/java/com/weicools/fluentlayout/MainConstraintMultiLayout2.kt
+++ app/src/main/java/com/weicools/fluentlayout/MainConstraintMultiLayout2.kt
@@ -29,7 +29,6 @@ import com.weicools.fluent.widget.params.wrapContent
 class MainConstraintMultiLayout2 : ConstraintLayout {
   constructor(context: Context) : super(context)
   constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
-  constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
   init {
     createView()
