3a95ad9c609542c3fbc82d22d6253a727ba3dedc
Weiwei <1570682285@qq.com>
2023-05-16 00:21:49 +0800

    rename app context dp->px

--- library/src/main/kotlin/com/weikeet/ui/app/unit/AppUnit.kt
+++ library/src/main/kotlin/com/weikeet/ui/app/unit/AppUnit.kt
@@ -3,5 +3,5 @@ package com.weikeet.ui.app.unit
 import com.weikeet.ui.app.mainContext
 
 // Use app context Resources
-fun Int.dpOf(value: Int): Int = (value * mainContext.resources.displayMetrics.density + 0.5f).toInt()
-fun Int.dpOf(value: Float): Float = value * mainContext.resources.displayMetrics.density
+fun dpOf(value: Int): Int = (value * mainContext.resources.displayMetrics.density + 0.5f).toInt()
+fun dpOf(value: Float): Float = value * mainContext.resources.displayMetrics.density
